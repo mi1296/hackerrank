19b9a407524a17afd1073717c6311f7b84aa8cb1
Madhumitha Ichapuram <1madhu2mitha@gmail.com>
2022-09-21 07:39:25 +0530

    Create staircase.py

---
+++ staircase.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(n):
+        for s in range(n-i-1):
+            print(" ", end='')
+        for j in range(i+1):
+            print("#", end='')
+        print()
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
